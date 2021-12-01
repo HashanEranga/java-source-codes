5d3c8de9cf5ec3ca3a448229be7d679cc951ca8e
Hashan Eranga <ccmrhe@gmail.com>
2021-12-01 14:57:18 +0530

    Initial commit

---
+++ Fundamentals/src/com/java/operators/codingEx2.java
@@ -0,0 +1,14 @@
+package com.java.operators;
+
+public class codingEx2 {
+
+	double Area;
+	public codingEx2(int base, int height) {
+		this.Area = 0.5 * base * height;
+	}
+	
+	public double returnArea() {
+		return Area;
+	}
+
+}

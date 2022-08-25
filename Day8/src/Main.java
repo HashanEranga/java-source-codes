ee87041e44fa079cfa3c9162048178ee4da6c463
HashanEPerera <45037796+HashanEranga@users.noreply.github.com>
2022-08-25 21:43:12 +0530

    static initialization Static variables and static methods

--- Day8/src/Main.java
+++ Day8/src/Main.java
@@ -1,20 +1,25 @@
-//private method
-class A{
-    int a;
-    private int b;
-    void mDef(){}
-    private void mPri(){}
-    public void mPub(){}
-
-    //private public default
-    public void myMethod(){
-        System.out.println(a);
-        System.out.println(b);
-        mPri();
-        mDef();
-        mPub();
-    }
+//static initialization (static variables and static methods)
+class MyClass {
+    int x; //instance variable
+    static int y = 10; // class variables, class variables and template variables.
 }
 
-//access modifiers
-//public private default protected
+class Example {
+    public static void main(String[] args) {
+        MyClass c1 = new MyClass();
+        c1.x = 1;
+        c1.y = 2;
+
+        MyClass c2 = new MyClass();
+        c2.x = 10;
+        c2.y = 20;
+
+        MyClass c3 = new MyClass();
+        c3.x = 100;
+        c3.y = 23;
+
+        System.out.println("C1 : " + c1.x + " " + c1.y);
+        System.out.println("C2 : " + c2.x + " " + c2.y);
+        System.out.println("C3 : " + c3.x + " " + c3.y);
+    }
+}

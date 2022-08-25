f0b2b71ff1d20fc25eb062f4286c6a3b28db6a10
HashanEPerera <45037796+HashanEranga@users.noreply.github.com>
2022-08-25 22:09:27 +0530

    null object show static variables

--- Day8/src/Main.java
+++ Day8/src/Main.java
@@ -1,3 +1,5 @@
+import java.sql.SQLOutput;
+
 class MyClass{
     int x = 100;
     static int y = 200;
@@ -10,5 +12,15 @@ class Example{
         System.out.println(a1.x);
         System.out.println(MyClass.y);
         // cannot access non-static variables using MyClass
+
+        MyClass a2 = null;
+        System.out.println(a2.y);
+        try{
+            System.out.println(a2.x);
+        }
+        catch (NullPointerException ex){
+            System.out.println("Error : Null pointer exception occured");
+            System.out.println("Error : " + ex.getMessage().toString());
+        }
     }
 }

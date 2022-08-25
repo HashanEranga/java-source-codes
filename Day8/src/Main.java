d217c92c03af885c5cb9745e689cc28dfa370547
HashanEPerera <45037796+HashanEranga@users.noreply.github.com>
2022-08-25 21:24:53 +0530

    call by value call by reference and pass by value

--- Day8/src/Main.java
+++ Day8/src/Main.java
@@ -10,13 +10,21 @@ class ChangeValue{
     public static void increaseValue(int a){
         a++;
     }
+
+    public static void increaseValueByRef(Value v){
+        v.a++;
+    }
 }
 
 class Demo {
     public static void main(String[] args) {
         Value v1 = new Value(100);
         System.out.println(v1.a);
+        // call by value
         ChangeValue.increaseValue(v1.a);
+        System.out.println(v1.a);
+//        call by reference
+        ChangeValue.increaseValueByRef(v1);
         System.out.println(v1.a);
     }
 }

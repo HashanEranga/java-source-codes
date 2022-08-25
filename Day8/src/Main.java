9de43edd8551b2df285132916452714490f518a4
HashanEPerera <45037796+HashanEranga@users.noreply.github.com>
2022-08-25 22:13:12 +0530

    static block instance block constuctor order

--- Day8/src/Main.java
+++ Day8/src/Main.java
@@ -1,26 +1,25 @@
-import java.sql.SQLOutput;
+class MyClass {
 
-class MyClass{
-    int x = 100;
-    static int y = 200;
+    static {
+        System.out.println("static block");
+    }
+
+    {
+        System.out.println("instance block");
+    }
+
+    MyClass(){
+        System.out.println("Constructor block");
+    }
 }
 
 class Example{
     public static void main(String[] args) {
         MyClass a1 = new MyClass();
-
-        System.out.println(a1.x);
-        System.out.println(MyClass.y);
-        // cannot access non-static variables using MyClass
-
-        MyClass a2 = null;
-        System.out.println(a2.y);
-        try{
-            System.out.println(a2.x);
-        }
-        catch (NullPointerException ex){
-            System.out.println("Error : Null pointer exception occured");
-            System.out.println("Error : " + ex.getMessage().toString());
-        }
+        System.out.println("==========================");
+        MyClass a2 = new MyClass();
     }
 }
+
+//static block run once when the class loads in to the ran and not after other objects are initialized
+//instance block runs at every time an object is created
